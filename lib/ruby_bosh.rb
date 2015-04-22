876deaaefd4ae36b5e38297fd273e683a59a72d7
Teodor Pripoae <teodor.pripoae@gmail.com>
2015-04-22 20:32:32 +0300

    Accept more headers for prebind

--- lib/ruby_bosh.rb
+++ lib/ruby_bosh.rb
@@ -4,7 +4,7 @@ require 'rexml/document'
 require 'base64'
 require 'hpricot'
 
-class RubyBOSH  
+class RubyBOSH
   BOSH_XMLNS    = 'http://jabber.org/protocol/httpbind'
   TLS_XMLNS     = 'urn:ietf:params:xml:ns:xmpp-tls'
   SASL_XMLNS    = 'urn:ietf:params:xml:ns:xmpp-sasl'
@@ -23,7 +23,7 @@ class RubyBOSH
   end
 
   attr_accessor :jid, :rid, :sid, :success , :custom_resource
-  def initialize(jid, pw, service_url, opts={}) 
+  def initialize(jid, pw, service_url, opts={})
     @service_url = service_url
     # Extract the resource if present
     split_jid = jid.split("/")
@@ -32,9 +32,11 @@ class RubyBOSH
     @pw = pw
     @host = @jid.split("@").last
     @success = false
-    @timeout = opts[:timeout] || 3 #seconds 
-    @headers = {"Content-Type" => "text/xml; charset=utf-8",
-                "Accept" => "text/xml"}
+    @timeout = opts[:timeout] || 3 #seconds
+    @headers = opts.fetch(:headers, {}).merge({
+                                                "Content-Type" => "text/xml; charset=utf-8",
+                                                "Accept" => "text/xml"
+                                              })
     @wait    = opts[:wait]   || 5
     @hold    = opts[:hold]   || 3
     @window  = opts[:window] || 5
@@ -50,7 +52,7 @@ class RubyBOSH
 
   def connect
     initialize_bosh_session
-    if send_auth_request 
+    if send_auth_request
       send_restart_request
       request_resource_binding
       @success = send_session_request
@@ -58,12 +60,12 @@ class RubyBOSH
 
     raise RubyBOSH::AuthFailed, "could not authenticate #{@jid}" unless success?
     @rid += 1 #updates the rid for the next call from the browser
-    
+
     [@jid, @sid, @rid]
   end
 
   private
-  def initialize_bosh_session 
+  def initialize_bosh_session
     response = deliver(construct_body(:wait => @wait, :to => @host,
                                       :hold => @hold, :window => @window,
                                       "xmpp:version" => '1.0'))
@@ -75,7 +77,7 @@ class RubyBOSH
 
     builder = Builder::XmlMarkup.new
     parameters = {:rid => @rid, :xmlns => BOSH_XMLNS,
-                  "xmpp:version" => "1.0", 
+                  "xmpp:version" => "1.0",
                   "xmlns:xmpp" => "urn:xmpp:xbosh"}.merge(params)
 
     if block_given?
@@ -85,10 +87,10 @@ class RubyBOSH
     end
   end
 
-  def send_auth_request 
+  def send_auth_request
     request = construct_body(:sid => @sid) do |body|
-      auth_string = "#{@jid}\x00#{@jid.split("@").first.strip}\x00#{@pw}" 
-      body.auth(Base64.encode64(auth_string).gsub(/\s/,''), 
+      auth_string = "#{@jid}\x00#{@jid.split("@").first.strip}\x00#{@pw}"
+      body.auth(Base64.encode64(auth_string).gsub(/\s/,''),
                     :xmlns => SASL_XMLNS, :mechanism => 'PLAIN')
     end
 
@@ -103,16 +105,16 @@ class RubyBOSH
 
   def request_resource_binding
     request = construct_body(:sid => @sid) do |body|
-      body.iq(:id => "bind_#{rand(100000)}", :type => "set", 
+      body.iq(:id => "bind_#{rand(100000)}", :type => "set",
               :xmlns => "jabber:client") do |iq|
         iq.bind(:xmlns => BIND_XMLNS) do |bind|
           bind.resource(resource_name)
         end
       end
     end
-    
+
     response = deliver(request)
-    response.include?("<jid>") 
+    response.include?("<jid>")
   end
 
   def send_session_request
@@ -120,11 +122,11 @@ class RubyBOSH
       body.iq(:xmlns => CLIENT_XMLNS, :type => "set",
               :id => "sess_#{rand(100000)}") do |iq|
         iq.session(:xmlns => SESSION_XMLNS)
-      end 
+      end
     end
 
     response = deliver(request)
-    response.include?("body") 
+    response.include?("body")
   end
 
   def parse(_response)
@@ -138,7 +140,7 @@ class RubyBOSH
   begin
     require 'system_timer'
     def deliver(xml)
-      SystemTimer.timeout(@timeout) do 
+      SystemTimer.timeout(@timeout) do
         send(xml)
         recv(RestClient.post(@service_url, xml, @headers))
       end
@@ -156,7 +158,7 @@ class RubyBOSH
     def deliver(xml)
       ::Timeout::timeout(@timeout) do
         send(xml)
-        recv(RestClient.post(@service_url, xml, @headers))            
+        recv(RestClient.post(@service_url, xml, @headers))
       end
     rescue ::Timeout::Error => e
       raise RubyBOSH::Timeout, e.message
@@ -176,7 +178,7 @@ class RubyBOSH
   end
 
   private
-  def now 
+  def now
     Time.now.strftime("%a %b %d %H:%M:%S %Y")
   end
 
@@ -191,6 +193,6 @@ end
 
 
 if __FILE__ == $0
-  p RubyBOSH.initialize_session(ARGV[0], ARGV[1], 
+  p RubyBOSH.initialize_session(ARGV[0], ARGV[1],
       "http://localhost:5280/http-bind")
 end
